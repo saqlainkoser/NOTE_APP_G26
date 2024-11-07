ceb6cfa8d27ba25a3e4d364559cbdf16ef8c54c0
saqlainkoser <saqlainkoser@gmail.com>
2024-11-07 19:50:13 +0530

    its me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signup from './Pages/Signup'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/signup' element={<Signup/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+     <div className="navbar h-[90px] bg-[#f4f4f4] flex items-center justify-between w-screen px-[50px]">
+        <h1 className='logo text-2xl'>Keep Notes</h1>
+        </div> 
+    </>
+  )
+}
+
+export default Navbar
