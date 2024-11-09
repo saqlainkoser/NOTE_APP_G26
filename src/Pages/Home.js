adf46ca5dc5f1ac1559b5f49fa8f5340a6dc522e
saqlainkoser <saqlainkoser@gmail.com>
2024-11-09 19:34:15 +0530

    its mee

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Note from '../components/Note'
 
 function Home() {
   return (
     <>
       <Navbar/>
+      <div className="mt-[20px] flex items-center justify-between w-screen px-[50px]">
+        <h1 className='text-2xl'>Hi, Shaan</h1>
+        <div className="inputBox !w-[350px]">
+            <input type="text" placeholder='Search Notes' className='!p-[10px]'/>
+        </div>
+      </div>
+      <div className="gridItems">
+          <Note/>
+          <Note/>
+          <Note/>
+          <Note/>
+        </div>
     </>
   )
 }
